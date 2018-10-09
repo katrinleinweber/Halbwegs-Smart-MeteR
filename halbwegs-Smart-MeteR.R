1dbd8b8ea2ec884ff327f0a18be6c21c48f2f8cb
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2018-10-09 20:34:29 +0200

    Erstelle Aufgaben & Zwischenziele

---
+++ halbwegs-Smart-MeteR.R
@@ -0,0 +1,33 @@
+# Ziel: Ein R-Script bauen, das mit dem aktuellen Datum und dem Zählerstand
+# aufgerufen werden kann, beide Datenpunkte einer Tabelle hinzufügt
+# und in ein Diagramm den Stromverbrauchs gegen die Zeit aufträgt.
+
+# definiere Variablen
+
+# lese Datensatz ein
+# install.packages("readr")
+
+# prüfe Datentyp & -struktur
+
+# visualisiere Zählerstand
+
+# füge weiteren Zählerstand hinzu
+
+# speichere aktualisierte Tabelle als Datei
+
+# breche Verbrauch auf Tage herunter & rechne auf Monat hoch
+#
+# nötige Berechnungen:
+# 1. [ ] aus den Datumsangaben ein Intervall
+# 2. [ ] Stromverbrauch zwischen zwei Zählerständen
+# 3. [ ] Verbrauch pro Tag
+# 4. [ ] Abschlag aus Grundpreis, kWh/Tag * 30 * Preis/kWh
+
+
+# visualisiere Hochrechnung & hübsche auf
+
+  # hochgerechnet auf Monat
+
+  # hochgerechnet auf Jahr
+
+  # tatsächlich
