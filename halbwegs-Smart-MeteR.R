dfd49218c44c1a3a03860f172df1211273ff0c14
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2018-11-15 17:26:06 +0100

    Aktualisiere Kommentare

--- halbwegs-Smart-MeteR.R
+++ halbwegs-Smart-MeteR.R
@@ -1,15 +1,14 @@
-# Ziel: Ein R-Script bauen, das mit dem aktuellen Datum und dem Zählerstand
+### Ziel: Ein R-Script bauen, das mit dem aktuellen Datum und dem Zählerstand
 # aufgerufen werden kann, beide Datenpunkte einer Tabelle hinzufügt
 # und in ein Diagramm den Stromverbrauch gegen die Zeit aufträgt.
 
 # lese Datensatz ein
 # install.packages("readr")
+# visualisiere Zählerstand (siehe auch Help > Cheatsheets)
+# install.packages("ggplot2")
 
-# prüfe Datentyp & -struktur
-
-# visualisiere Zählerstand
-
-# füge weiteren Zählerstand hinzu
+# füge weiteren Zählerstand zur Tabelle hinzu
+# install.packages("tibble")
 
 # speichere aktualisierte Tabelle als Datei
 
@@ -17,24 +16,31 @@
 ### weiteres Ziel: Aus dem bisher erfassten Verbrauch den Abschlag hochrechnen
 # und visualisieren.
 
-# definiere Variablen
-
-# lese Datensatz neu ein (und somit alle Daten einheitlich)
+# definiere Variablen (im Vertragssinne eher "Konstanten")
 
 # breche Verbrauch auf Tage herunter & rechne auf Monat hoch
-#
-# nötige Berechnungen:
-# 1. [ ] aus den Datumsangaben ein Intervall
-# 2. [ ] Stromverbrauch zwischen zwei Zählerständen
-# 3. [ ] Verbrauch pro Tag
-# 4. [ ] Abschlag aus Grundpreis, kWh/Tag * 30 * Preis/kWh
+# 1. aus den Datumsangaben ein Intervall
+# 2. Stromverbrauch zwischen zwei Zählerständen
+# 3. Stromverbrauch pro Tag
+# 4. realistischer Abschlag aus Grundpreis, kWh/Tag * 30 * Preis/kWh
 
 # install.packages("dplyr")
 
-# visualisiere Hochrechnung & hübsche auf
+
+
+# visualisiere Hochrechnung
+
 
   # hochgerechnet auf Monat
 
   # hochgerechnet auf Jahr
 
   # tatsächlich
+
+  # aufhübschen (zusätzlich zu linetype & color oben)
+
+
+### Eigene Weiterarbeit hieran: Beispieldaten aus Strom.csv löschen, dieses die
+# .RProj-Datei regelmäßig öffnen, neuen Zählerstand in add_row(...) eintragen
+# und mittels "Source"-Button ausführen.
+
